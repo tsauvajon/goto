6693a3e072a8e5814c6d9d2223cb50b2af6bc75a
Thomas Sauvajon <thomas@sauvajon.tech>
2021-06-30 19:15:51 +0200

    Update the e2e test to use the CLI

--- e2e.sh
+++ e2e.sh
@@ -7,7 +7,7 @@ killall goto-api &> /dev/null || true
 cargo build
 target/debug/goto-api --addr ${ADDR} &
 
-goto='target/debug/goto-cli'
+cargo build --bin goto
 
 echo 'Waiting for the GoTo API to be up and running'
 for i in {1..20}
@@ -16,20 +16,32 @@ do
     sleep 1s
 done
 
-echo "Starting tests"
+echo -e 'Starting tests\n'
+
+echo 'creating /tsauvajon with a direct HTTP query'
 curl --silent --show-error --fail -X POST $ADDR/tsauvajon -d "https://linkedin.com/in/tsauvajon" > /dev/null
-curl --silent --show-error --fail $ADDR/tsauvajon > /dev/null
+echo -e ' -> ok\n'
+echo 'browsing /tsauvajon with the CLI'
+target/debug/goto tsauvajon --no-open-browser --api http://${ADDR} | grep -q 'redirecting to https://linkedin.com/in/tsauvajon' || (echo ' -> failed!' && exit 2)
+echo -e ' -> ok\n'
 
-curl --silent --show-error --fail -X POST $ADDR/hello -d "http://world" > /dev/null
-curl --silent --show-error --fail $ADDR/hello > /dev/null
+echo 'creating /hello with the CLI'
+target/debug/goto hello http://hello.world --api http://${ADDR}
+echo -e ' -> ok\n'
+echo 'browsing /hello with a direct HTTP query'
+curl --silent --show-error --fail $ADDR/hello | grep -q 'redirecting to http://hello.world' || (echo ' -> failed!' && exit 3)
+echo -e ' -> ok\n'
 
+echo 'querying inexisting short URL'
 http_status=$(curl --silent -w "%{http_code}" $ADDR/qwertyuiop)
 
 if [[ $http_status != "not found404" ]]; then
-    echo "expected status 404, got $http_status"
-    exit 2
+    echo " -> expected status 404, got '$http_status'"
+    echo ' -> failed!'
+    exit 404
 fi
 
-echo "SUCCESS"
+echo -e ' -> ok\n'
+echo 'SUCCESS'
 
 killall goto-api
